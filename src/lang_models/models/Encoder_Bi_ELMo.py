33755ebee771852b71e22be526a2911d012d9d1b
hhhhzy <46476528+hhhhzy@users.noreply.github.com>
2020-11-29 00:34:51 -0500

    Update Encoder_Bi_ELMo.py

--- src/lang_models/models/Encoder_Bi_ELMo.py
+++ src/lang_models/models/Encoder_Bi_ELMo.py
@@ -64,10 +64,7 @@ class EncoderRNN(BaseRNN):
         """
         embedded = self.embedding(input_var)
         embedded = self.input_dropout(embedded)
-        if self.variable_lengths:
-            embedded = nn.utils.rnn.pack_padded_sequence(embedded, input_lengths, batch_first=True)
         output, hidden = self.rnn(embedded)
-        if self.variable_lengths:
-            output, _ = nn.utils.rnn.pad_packed_sequence(output, batch_first=True)
+
         return output, hidden
 
