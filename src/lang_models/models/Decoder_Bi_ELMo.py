9bdba165c9caef98f8511d41f03bd638cfc29bfc
hhhhzy <46476528+hhhhzy@users.noreply.github.com>
2020-11-28 23:36:42 -0500

    Update Decoder_Bi_ELMo.py

--- src/lang_models/models/Decoder_Bi_ELMo.py
+++ src/lang_models/models/Decoder_Bi_ELMo.py
@@ -13,7 +13,7 @@ import torch.nn as nn
 from torch.autograd import Variable
 import torch.nn.functional as F
 
-from allennlp.commands.elmo import ElmoEmbedder
+from allennlp.modules.elmo import Elmo
 
 from .attention import Attention
 from .baseRNN import BaseRNN
@@ -87,8 +87,11 @@ class DecoderRNN(BaseRNN):
         self.sos_id = sos_id
 
         self.init_input = None
-
-        self.embedding = ElmoEmbedder()
+        options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_options.json"
+        weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway/elmo_2x4096_512_2048cnn_2xhighway_weights.hdf5"
+        elmo = Elmo(options_file, weight_file, 1, dropout=0)
+        self.embedding = elmo
+        
         if use_attention:
             self.attention = Attention(self.hidden_size, model_name)
 
