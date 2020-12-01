71ffce29feaf28b0c8b0f3544936a07bc4b926fd
hhhhzy <46476528+hhhhzy@users.noreply.github.com>
2020-12-01 00:24:16 -0500

    Update seq2seq.py

--- src/lang_models/models/seq2seq.py
+++ src/lang_models/models/seq2seq.py
@@ -34,12 +34,16 @@ class Seq2Seq(nn.Module):
         else:
             self.question_encoder = EncoderRNN(vocab_size=vocab_size, max_len=max_len, embedding_size=e_size,
                         hidden_size=q_hsize, n_layers=n_layers, dropout_p=dropout_p, bidirectional=bidirectional, rnn_cell=rnn_cell)
+            """
             self.decoder.embedding.weight = self.question_encoder.embedding.weight
+            """
 
         if model_name == C.LM_QUESTION_ANSWERS_REVIEWS:
             self.reviews_encoder = EncoderRNN(vocab_size=vocab_size, max_len=max_len, embedding_size=e_size,
                         hidden_size=r_hsize, n_layers=n_layers, dropout_p=dropout_p, bidirectional=bidirectional, rnn_cell=rnn_cell)
+            """
             self.decoder.embedding.weight = self.reviews_encoder.embedding.weight
+            """
         else:
             self.reviews_encoder = None
 
